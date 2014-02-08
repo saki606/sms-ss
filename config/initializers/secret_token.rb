73a05ea6809f42f53e21aae15b24cf09264ba2f2
Go Fujita <saki606@gmail.com>
2014-02-09 06:38:59 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmsSs::Application.config.secret_key_base = '1443500d002e58b62851db519f0b84b09649c5593847776ff87434c37bd8cf282e988436bc868d1004df88c49136eb410f127afffec02bee5bdebc5ef26a0298'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmsSs::Application.config.session_store :cookie_store, key: '_sms_ss_session'
